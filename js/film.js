351b77e2489e96c7871d8cc05494b7d2276f94b8
nadiafigo <nadia.figueroa93@gmail.com>
2022-08-03 23:19:01 -0500

    movie details html and js files

--- js/film.js
+++ js/film.js
@@ -1,11 +1,37 @@
 let url = 'https://ghibliapi.herokuapp.com/films'
 
-const divFilmTitle = document.getElementById('filmheader');
-const divFilmImg = document.getElementById('film-picture');
+const h1FilmTitle = document.getElementById('film-title')
+const filmYear = document.getElementById('year')
+const filmImg = document.getElementById('film-picture');
 const divAudienceStars = document.getElementById('star-rating__audience');
 const allStars = document.querySelectorAll('.star');
-const divDescription = document.getElementById('film-description');
+const description = document.getElementById('film-description');
+const moreInfo = document.getElementById('more-info')
+
+let params = new URLSearchParams(document.location.search);
+let film_id = params.get("film_id");
+console.log(film_id)
 
 
-fetch(url)
-.then
+fetch(url + "/" + film_id)
+.then((text) => text.json())
+.then((movie) => {
+    console.log(movie)
+    h1FilmTitle.innerText = movie.title
+    filmYear.innerText = movie.release_date
+    filmImg.setAttribute('src', movie.image)
+    
+    audienceScore = ''
+    audienceScore = `
+    <button class="star">${(movie.rt_score >= 20)? "&#9733" : "&#9734"}</button>
+    <button class="star">${(movie.rt_score >= 40)? "&#9733" : "&#9734"}</button>
+    <button class="star">${(movie.rt_score >= 60)? "&#9733" : "&#9734"}</button>
+    <button class="star">${(movie.rt_score >= 80)? "&#9733" : "&#9734"}</button>
+    <button class="star">${(movie.rt_score >= 100)? "&#9733" : "&#9734"}</button>`
+
+    divAudienceStars.innerHTML = audienceScore
+
+    description.innerText = movie.description
+
+    moreInfo.setAttribute('href', movie.url)
+});
