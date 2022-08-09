7593e87f1e06bf96319fbd570b73830aedc5076a
nadiafigo <nadia.figueroa93@gmail.com>
2022-08-08 20:36:21 -0500

    filter function added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,13 @@
 let url = 'https://ghibliapi.herokuapp.com/films'
 
 const divFilmCards = document.getElementById('filmlist-container');
-const filmCardTemplate = document.querySelector('[film-card-template]')
+const filmCardTemplate = document.querySelector('[film-card-template]');
 
 const searchInput = document.getElementById('search__input');
+
+const sortFilter = document.getElementById('sort-by');
+const orderFilter = document.getElementById('order')
+
 
 
 let films = []
@@ -19,6 +23,7 @@ fetch(url)
             const checkmarkInput = card.querySelector("[checkmark-input]")
             const checkmarkLabel = card.querySelector("[checkmark-label]")
             const filmYear = card.querySelector("[film-year]")
+            const filmDuration = card.querySelector("[duration]")
             const filmDescription = card.querySelector("[film-description]")
             const filmRating = card.querySelector("[star-rating]")
             enlace.setAttribute('href', "./html/film.html?film_id=" + film.id)
@@ -28,6 +33,7 @@ fetch(url)
             checkmarkInput.setAttribute('id', "film__checkbox" + i)
             checkmarkLabel.setAttribute('for', "film__checkbox" + i)
             filmYear.innerText = film.release_date
+            filmDuration.innerText = film.running_time + " min"
             filmDescription.innerText = film.description
             filmRating.innerHTML = `<button class="h_star">${(film.rt_score >= 20)? "&#9733" : "&#9734"}</button>
             <button class="h_star">${(film.rt_score >= 36)? "&#9733" : "&#9734"}</button>
@@ -47,18 +53,81 @@ fetch(url)
         films.forEach(film => {
             let searchItem = ""
             searchItem = film.title.toLowerCase()
-            console.log(searchItem)
-            if(searchItem.includes(value)) {
-                film.element.classList.add('visible')
-                film.element.classList.remove('hide')
-            } else {
-                film.element.classList.add('hide')
-                film.element.classList.remove('visible')
-            }
+            searchItem.includes(value) ? film.element.classList.remove('hide') : film.element.classList.add('hide');
         })
-    
+
     })
 
+
+function Filter(sortSelected, orderSelected) {
+    if(sortSelected == "Duration") {
+        films.sort((a, b) => {
+            if(orderSelected == "Descendant") {
+                return b.duration - a.duration;
+            }
+            return a.duration - b.duration;
+    })
+    }
+
+    if(sortSelected == "Year") {
+        films.sort((a, b) => {
+            if(orderSelected == "Descendant") {
+                return b.year - a.year;
+            }
+            return a.year - b.year;
+    })
+    } 
+
+    if(sortSelected == "Audience Score") {
+        films.sort((a, b) => {
+            if(orderSelected == "Descendant") {
+                return b.audienceScore - a.audienceScore;
+            }
+            return a.audienceScore - b.audienceScore;
+    })} 
+
+    divFilmCards.innerHTML = "";
+    films.forEach(film => {
+        divFilmCards.append(film.element)})
+}
+
+
+    sortFilter.addEventListener("change", (e) => {
+        orderSelected = orderFilter.value
+        sortSelected = e.target.value
+        Filter(sortSelected, orderSelected)
+    });
+
+    orderFilter.addEventListener("change", (e) => {
+        sortSelected = sortFilter.value
+        orderSelected = e.target.value
+        Filter(sortSelected, orderSelected)
+    });
+
+
+
+
+        // AscendantOrder.addEventListener("click", f => {
+        //     films.sort((a, b) => {
+        //         return a.duration - b.duration;
+        //     })
+            
+        // })
+        
+        // DescendantOrder.addEventListener("click", f => {
+        //     films.sort((a, b) => {
+        //         return b.duration - a.duration;
+        //     })
+        // })
+
+
+    // if(searchItem.includes(value)) {
+    //     film.element.classList.add('visible')
+    //     film.element.classList.remove('hide')
+    // } else {
+    //     film.element.classList.add('hide')
+    //     film.element.classList.remove('visible')
+    // }
 
 // fetch(url) 
 //     .then((text) => text.json())
