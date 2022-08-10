2e3b29232088c4af73965f5d2ddc6df0d3264d4a
nadiafigo <nadia.figueroa93@gmail.com>
2022-08-10 15:47:09 -0500

    firebase auth and storing init

---
+++ js/firebase.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-analytics.js";
+import { } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-firestore.js"
+import { } from "https://www.gstatic.com/firebasejs/9.9.2/firebase-auth.js"
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyBMWj_7CjLgoiBzqaoqCugI8JFP1xk-BUY",
+authDomain: "ghibli-webapp.firebaseapp.com",
+projectId: "ghibli-webapp",
+storageBucket: "ghibli-webapp.appspot.com",
+messagingSenderId: "555389541360",
+appId: "1:555389541360:web:8c7b3acb7da22f005d1b35",
+measurementId: "G-Q1YCEX8LZL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
